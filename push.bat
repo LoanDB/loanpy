c1b6e072ab3342da602f2f5d3ed18429d368e832
martino_vic <viktor.martinovic@hotmail.com>
2020-10-17 23:48:16 +0200

    v 0.1.5 on pypi

--- push.bat
+++
@@ -1,6 +0,0 @@
-cd C:\Users\Viktor\OneDrive\PhD cloud\Git\Framework-for-computer-aided-borrowing-detection-master
-git init
-git add .
-git commit -m "v 0.1.3 on pypi"
-git remote add origin https://github.com/martino-vic/Framework-for-computer-aided-borrowing-detection.git
-git push -f origin master
