d9dca7000f3a6444ae4a77177a0d894cad2119c0
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-03-30 17:54:21 +0200

    [skip ci] remove src folder

--- loanpy/__init__.py
+++ loanpy/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-.. image:: ../../docs/source/images/white_logo_dark_background.jpg
+.. image:: ../../docs/images/white_logo_dark_background.jpg
    :scale: 20%
 
 loanpy is a toolkit for solving various linguistic tasks such as:
@@ -13,13 +13,13 @@ For an installation guide, click on the Cheese Shop logo. To see the latest \
 but perhaps not the most stable version, click on Octocat. \
 For citation visit Zenodo:
 
-.. image:: ../../docs/source/images/PyPI_logo.svg
+.. image:: ../../docs/images/PyPI_logo.svg
    :scale: 50%
    :target: https://pypi.org/project/loanpy/
-.. image:: ../../docs/source/images/Octocat.png
+.. image:: ../../docs/images/Octocat.png
    :scale: 20%
    :target: https://github.com/martino-vic/loanpy
-.. image:: ../../docs/source/images/zenodo-gradient-200.png
+.. image:: ../../docs/images/zenodo-gradient-200.png
    :scale: 50%
    :target: https://zenodo.org/record/4127115#.YHCQwej7SLQ
 
