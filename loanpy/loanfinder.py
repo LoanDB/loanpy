cc4235d4d6451bdc1b99f7afe72e3fd0743ff433
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-04-26 00:48:12 +0200

    [skip ci] docs: update examplese in docstrings in loanfinder

--- loanpy/loanfinder.py
+++ loanpy/loanfinder.py
@@ -56,6 +56,24 @@ def phonetic_matches(
              with the following columns:
              ID, loanID, adrcID, df, form, predicted, meaning.
     :rtype: str
+
+    .. code-block:: python
+
+    >>> from loanpy.loanfinder import phonetic_matches
+    >>> donor = [
+    >>>     ['a0', 'f0', 'igig'],
+    >>>     ['a1', 'f1', 'iggi']
+    >>> ]
+    >>> recipient = [
+    >>>     ['0', 'Recipientese-0', '^(i|u)(g)(g)(i|u)$'],
+    >>>     ['1', 'Recipientese-1', '^(i|u)(i|u)(g)(g)$']
+    >>> ]
+    >>> outpath = "examples/phonetic_matches.tsv"
+    >>> phonetic_matches(recipient, donor, outpath)
+    >>> with open(outpath, "r") as f:
+    >>>     print(f.read())
+    ID	ID_rc	ID_ad
+    0	Recipientese-0	f1
     """
     phmid = 0
     with open(output, "w+") as f:
